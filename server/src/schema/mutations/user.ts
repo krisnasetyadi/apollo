f051cf433b428c50d354ec6b3d6a364b7684f8d8
krisna dwi setyaadi <krisnadwisetyadi@gmail.com>
2023-05-28 11:46:10 +0700

    update and delete method

--- server/src/schema/mutations/user.ts
+++ server/src/schema/mutations/user.ts
@@ -1,7 +1,7 @@
-import { GraphQLString } from "graphql";
+import { GraphQLID, GraphQLString } from "graphql";
 import { UserType } from "../types/user";
 import { Users } from "../../entities/users";
-
+import { MessageType } from "../types/messages";
 export const CREATE_USER = {
     type: UserType,
     args: {
@@ -17,5 +17,46 @@ export const CREATE_USER = {
             password
         })
         return args
+    }
+}
+
+export const UPDATE_PASSWORD = {
+    type: MessageType,
+    args: {
+        username: {type: GraphQLString},
+        oldPassword: {type: GraphQLString},
+        newPassword: {type: GraphQLString}
+    },
+    async resolve(parent: any, args: any) {
+        const { username, oldPassword, newPassword  } = args
+        const user = await Users.findOne({ username: username })
+        if(!user) {
+            throw new Error('Username doesnt exist') 
+        }
+        const userPassword = user?.password
+       
+        if (oldPassword === userPassword) {
+            await Users.update(
+             { username: username }, 
+             { password: newPassword })
+            return { successfull: true, message: 'password updated'  }
+        } else {
+            throw new Error('Password do not match')
+        }
+    }
+}
+
+export const DELETE_USER = {
+    type: MessageType,
+    args: {
+        id: { type: GraphQLID  }
+    },
+    async resolve(parent: any, args: any) {
+       const id = args.id
+       await Users.delete(id)
+       return  {
+        succesfull: true,
+        message: 'Delete successfull'
+       }
     }
 }
